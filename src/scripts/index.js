9fae8daa5f7ea53e4465a00ae7b08daa841e0e65
dmko1610 <trigger1610@gmail.com>
2019-08-15 02:37:02 +0400

    added event listener, some styles.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+console.log("WHAAT");
+
+document.querySelector(".button-submit").addEventListener("click", function () {
+    let login = document.querySelector('.email-field').value;
+    let pass = document.querySelector('.password-field').value;
+    console.log(login);
+    console.log(pass);
+});
